7ad234657268059c02bff8fc0570bf2a727fa622
Zotoavina <zotoavinanantenaina@gmail.com>
2023-04-06 13:40:26 +0200

    Écriture d'un EJB session stateless GestionnaireCompte et déclaration de la source de données

---
+++ src/main/java/mg/itu/tpbanquerasoaharisoa/ejb/GestionnaireCompte.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpbanquerasoaharisoa.ejb;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.ejb.Stateless;
+
+/**
+ *
+ * @author ASUS
+ */
+@DataSourceDefinition (
+    className="com.mysql.cj.jdbc.MysqlDataSource",
+    name="java:app/jdbc/banque",
+    serverName="localhost",
+    portNumber=3306,
+    user="root",              
+    password="j@kartaE3",
+    databaseName="banque",
+    properties = {
+      "useSSL=false",
+      "allowPublicKeyRetrieval=true"
+    }
+)
+@Stateless
+public class GestionnaireCompte {
+    
+}
