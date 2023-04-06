a78127d9eb245c6e75c42dab9cebc1452cb7fbec
Zotoavina <zotoavinanantenaina@gmail.com>
2023-04-06 13:34:27 +0200

    Création et modification de l'entité CompteBancaire

---
+++ src/main/java/mg/itu/tpbanquerasoaharisoa/entities/CompteBancaire.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpbanquerasoaharisoa.entities;
+
+import java.io.Serializable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ *
+ * @author ASUS
+ */
+@Entity
+public class CompteBancaire implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    
+    private int solde;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getSolde() {
+        return solde;
+    }
+
+    public void setSolde(int solde) {
+        this.solde = solde;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public CompteBancaire(){}
+    
+    public CompteBancaire(String nom, int solde){
+        this.nom = nom;
+        this.solde = solde;
+    }
+    
+    public void deposer(int montant){
+        solde += montant;
+    }
+    
+    public void retirer(int montant){
+        if(solde < montant) solde = 0;
+        else solde -= montant;
+    }
+
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof CompteBancaire)) {
+            return false;
+        }
+        CompteBancaire other = (CompteBancaire) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mg.itu.tpbanquerasoaharisoa.entities.CompteBancaire[ id=" + id + " ]";
+    }
+    
+}
