3e91d450a4c21ca5bf8030c00b0cde32efd4f5a3
Zotoavina <zotoavinanantenaina@gmail.com>
2023-04-12 20:29:09 +0200

    Modification de la generation du clé dans OperationBancaire et ajout de l'association dans la classe entité CompteBancaire

--- src/main/java/mg/itu/tpbanquerasoaharisoa/entities/CompteBancaire.java
+++ src/main/java/mg/itu/tpbanquerasoaharisoa/entities/CompteBancaire.java
@@ -4,13 +4,18 @@
  */
 package mg.itu.tpbanquerasoaharisoa.entities;
 
+import jakarta.persistence.CascadeType;
 import java.io.Serializable;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.NamedQueries;
 import jakarta.persistence.NamedQuery;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -30,6 +35,9 @@ public class CompteBancaire implements Serializable {
     private String nom;
     
     private int solde;
+    
+    @OneToMany(cascade=CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<OperationBancaire> operations;
 
     public String getNom() {
         return nom;
@@ -49,6 +57,10 @@ public class CompteBancaire implements Serializable {
     
     public Long getId() {
         return id;
+    }
+    
+    public List<OperationBancaire> getOperations(){
+        return operations;
     }
     
     public CompteBancaire(){}
